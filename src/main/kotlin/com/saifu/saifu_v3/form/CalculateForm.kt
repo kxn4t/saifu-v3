e869f7293b014ac04ae1a53895a1c1a5815180a4
kxn4t <kxxn4t@gmail.com>
2020-08-12 18:53:21 +0900

    Change nullable to not null on form & some refactoring

--- src/main/kotlin/com/saifu/saifu_v3/form/CalculateForm.kt
+++ src/main/kotlin/com/saifu/saifu_v3/form/CalculateForm.kt
@@ -7,19 +7,20 @@ import javax.validation.constraints.Pattern
 data class CalculateForm(
         @field:NotEmpty(message = "必須です")
         @field:Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "日付形式で入力してください（yyyy-MM-dd）")
-        val from: String? = null,
+        val from: String = "",
         @field:NotEmpty(message = "必須です")
         @field:Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "日付形式で入力してください（yyyy-MM-dd）")
-        val to: String? = null,
-        var keyword: List<String>? = listOf("", "")
+        val to: String = "",
+        val keyword: List<String> = listOf("", "")
 ) {
     @AssertTrue(message = "必須です")
     fun isKeyword1NotEmpty(): Boolean {
-        return keyword?.get(0)?.isNotEmpty() ?: false
+        return keyword[0].isNotEmpty()
     }
 
     @AssertTrue(message = "必須です")
     fun isKeyword2NotEmpty(): Boolean {
-        return keyword?.get(1)?.isNotEmpty() ?: false
+        return keyword[1].isNotEmpty()
     }
+
 }
--- src/main/kotlin/com/saifu/saifu_v3/service/MoneyService.kt
+++ src/main/kotlin/com/saifu/saifu_v3/service/MoneyService.kt
@@ -17,19 +17,17 @@ class MoneyService(
     private val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
     private val zaimDateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
-    fun fetchAllMoneyList(): List<Money> = zaimClient.getMoneyList()
-
     fun fetchMoneyList(searchParameters: Map<MoneyConditionType, String> = mapOf()): List<Money> {
         return zaimClient.getMoneyList(searchParameters)
     }
 
     fun calculate(condition: CalculateForm): CalculateResult {
 
-        val fromDate = condition.from!!.toDate(dateTimeFormatter)
-        val toDate = condition.to!!.toDate(dateTimeFormatter)
+        val fromDate = condition.from.toDate(dateTimeFormatter)
+        val toDate = condition.to.toDate(dateTimeFormatter)
 
         // 期間内のMoneyListを取得
-        val calculateSource = fetchSixMonthsMoneyList().filter {
+        val calculateSource = fetchSixMonthsAgoMoneyList(fromDate).filter {
             it.created.toDate(zaimDateTimeFormatter).between(fromDate, toDate)
         }
 
@@ -38,13 +36,13 @@ class MoneyService(
 
         // キーワード毎に合計する
         val sumResults = mutableMapOf<String, Int>()
-        condition.keyword?.forEach { keyword ->
+        condition.keyword.forEach { keyword ->
             sumResults[keyword] = calculateSource.filter { it.comment == keyword }.sumBy { it.amount }
         }
 
         // キーワードに一致しないものをチェックする
         val wrongList = calculateSource.filter {
-            (it.comment != condition.keyword?.get(0)) && (it.comment != condition.keyword?.get(1))
+            !condition.keyword.contains(it.comment)
         }
         val wrongSum = wrongList.sumBy { it.amount }
 
@@ -54,7 +52,7 @@ class MoneyService(
         }
 
         // 差分計算して結果を格納
-        val average = sumResults.map { it.value }.sum() / 2
+        val average = sumResults.map { it.value }.sum() / condition.keyword.size
         val resultByKeyword = mutableListOf<CalculateResultByKeyword>()
         sumResults.forEach { (key, sum) ->
             val difference = average - sum
@@ -64,20 +62,19 @@ class MoneyService(
         return CalculateResult(total, wrongList, resultByKeyword)
     }
 
-    /** ここ半年間のものを取得する */
-    private fun fetchSixMonthsMoneyList(): List<Money> {
-        val sixMonthsAgo = LocalDate.now().minusMonths(6).format(dateTimeFormatter)
+    /** 集計期間の半年間前から取得する */
+    private fun fetchSixMonthsAgoMoneyList(fromDate: LocalDate): List<Money> {
+        val sixMonthsAgo = fromDate.minusMonths(6).format(dateTimeFormatter)
         val condition = mapOf(MoneyConditionType.START_DATE to sixMonthsAgo)
         return fetchMoneyList(condition)
     }
 
     private fun String.toDate(dateTimeFormatter: DateTimeFormatter): LocalDate {
-        return LocalDate.parse(this, dateTimeFormatter);
+        return LocalDate.parse(this, dateTimeFormatter)
     }
 
     fun LocalDate.between(from: LocalDate, to: LocalDate): Boolean {
-        return !(from.isAfter(this) || to.isBefore(this));
+        return !(from.isAfter(this) || to.isBefore(this))
     }
-
 
 }
